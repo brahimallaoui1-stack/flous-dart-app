4d5a15edeb6d5cf4acf7785ab99566fa07dbb5d4
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-08-05 17:03:28 +0000

    Se connecter avec Google ne fonction pas

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,9 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB-vSYikNziz3tVRpy3206z1XTrTiRJw80",
-  authDomain: "flous-dart-d9832.firebaseapp.com",
+  authDomain: "flous-dart-d9832.web.app",
   projectId: "flous-dart-d9832",
-  storageBucket: "flous-dart-d9832.firebasestorage.app",
+  storageBucket: "flous-dart-d9832.appspot.com",
   messagingSenderId: "407676989520",
   appId: "1:407676989520:web:c8413d4965ef6c0cd7a155"
 };
