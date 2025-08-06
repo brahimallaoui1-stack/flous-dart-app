1c5b5fc1ab27a7693fdf221e8d9f6bcffbd3314b
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-08-06 22:26:38 +0000

    L'application Web ne donne plus de fenêtre de chargement sur le téléphon

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,30 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 365 days
+        },
+      },
+    },
+     {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 365 days
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig: NextConfig = {
