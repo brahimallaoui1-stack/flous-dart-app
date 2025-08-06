5786246fb515c99217f295c01a6eae3d468654e5
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-08-06 15:04:17 +0000

    je veux changer l'icon de l'app est ce que c'est possible

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -11,14 +11,8 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-    <path d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
-    <path d="M12 12h.01" />
-    <path d="M22 12h-2" />
-    <path d="M14 14l5-5" />
-    <path d="M12 2v2" />
-    <path d="m7 7 5 5" />
-    <path d="M2 12h2" />
-    <path d="M12 22v-2" />
+    <circle cx="12" cy="12" r="10" />
+    <circle cx="12" cy="12" r="6" />
+    <circle cx="12" cy="12" r="2" />
   </svg>
 );
