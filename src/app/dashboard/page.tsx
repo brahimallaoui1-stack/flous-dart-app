6ffb69ef53a38f6e8f7d8b487717eb53d6a8635d
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-09-07 16:40:29 +0000

    annuler cette ettapeRuntime Error
    Error: AlertDialogTrigger is not def

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -35,6 +35,7 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
+    AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
 import { Label } from '@/components/ui/label';
 
