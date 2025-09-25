2c853d96d53bbfbd9a9d19c226fd4b3cfe3824e3
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-09-25 20:57:43 +0000

    Mon Profil (_for element span_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -87,7 +87,7 @@ export default function DashboardLayout({
                 <DropdownMenuItem asChild>
                   <Link href="/dashboard/profile">
                     <User className="mr-2 h-4 w-4" />
-                    <span>Profil</span>
+                    <span>Mon Profil</span>
                   </Link>
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
